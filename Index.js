0bbe6207b01138d5dd346b3698293ea3045a180b
Awais <Awaisbinmohsin@gmail.com>
2022-05-18 02:51:08 +0500

    Variables, Constants & Comments

---
+++ Index.js
@@ -0,0 +1,12 @@
+let age = 25;
+let year = 2022;
+console.log(age,year);
+age = 30; //We can use here let again
+console.log(age); //Now age is 30;
+const points = 100;
+console.log(points);
+points = 101; //It shows erros, consts cant be changed
+var score = 75; // var is older version
+console.log(score);
+// CamelCase , myAge, myYear are some example are camelCase
+// Comments are not use executed
